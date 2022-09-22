10befb2c7efd697c3d7861ff7af0e1552a239b42
Mringa School <lorna.mikatei@student.moringaschool.com>
2022-09-22 17:11:02 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser ='Lornah';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK,  'LORNAH!' ${currentUser}!` ;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!` ;
